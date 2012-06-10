b6fd730eca458e5f6464ed52717229dc8cd645d7
Clayton G. Hobbs <clay@lakeserv.net>
2012-06-10 14:26:13 -0400

    Gave ldt.sh command-line options
    It's getting to be quite the little brain-child.  It has a clean
    command (./ldt.sh clean) and the build command checks if it actually
    needs to build anything (by modification time, of course).  I'll
    need to add a force build option eventually, I realize.

--- ldt.sh
+++ ldt.sh
@@ -5,6 +5,10 @@ src="room"
 dest="htmlroom"
 files="*.md"
 
+function info {
+    echo -e "\033[1;32m$1\033[0m"
+}
+
 function note {
     echo -e "\033[36m$1\033[0m"
 }
@@ -18,42 +22,69 @@ function fatal {
     exit 1
 }
 
-echo -e "\033[1mL\033[0met's \033[1md\033[0mo" \
-    "\033[1mt\033[0mhis\033[1;30m.\033[37msh\033[0mit!"
-
-if [ ! -d "$src" ]; then
-    error "Source directory not found!"
-    fatal
-fi
-
-if [ ! -d "$dest" ]; then
-    note "Destination directory not found; creating it now."
-    mkdir "$dest"
-fi
-
-cwd=$PWD
-
-mod="$cwd/.ldtsh_last"
-cf="mod"
-if [ ! -f ".ldtsh_last" ]; then
-    cf="f"
-fi
-
-cd "$src"
-for f in $files; do
-    if [ -f "$f" -a ! `eval echo \\$$cf` -nt "$f" ]; then
-        g=`echo "$f" | sed 's/\.md//'`
-        cd "$cwd"
-        # Okay, yeah, this is a dirty hack, but it gets the job done without
-        # having to write the same command twice.
-        bash -xc "markdown_py \"$src/$g.md\" -f \"$dest/$g.html\""
-        if [ $? -ne 0 ]; then
-            error "Build error!"
-            fatal
-        fi
-        cd "$src"
+case $1 in
+  help)
+    echo -e "\033[1mL\033[0met's \033[1md\033[0mo" \
+        "\033[1mt\033[0mhis\033[1;30m.\033[37msh\033[0mit!"
+    echo "usage: ldt.sh [subcommand]"
+    echo "where subcommand is one of:"
+    echo "  build (default): compile the project"
+    echo "  clean: remove anything ldt.sh created"
+    echo "  help: show this message"
+    ;;
+  clean)
+    info "Let's clean this shit!"
+    if [ -d "$dest" ]; then
+        bash -xc "rm -r \"$dest\""
     fi
-done
+    if [ -f ".ldtsh_last" ]; then
+        bash -xc "rm .ldtsh_last"
+    fi
+    ;;
+  build | *)
+    info "Let's build this shit!"
 
-cd "$cwd"
-touch .ldtsh_last
+    if [ ! -d "$src" ]; then
+        error "Source directory not found!"
+        fatal
+    fi
+
+    if [ ! -d "$dest" ]; then
+        note "Destination directory not found; creating it now."
+        mkdir "$dest"
+    fi
+
+    cwd=$PWD
+
+    last="$cwd/.ldtsh_last"
+    cf="last"
+    if [ ! -f ".ldtsh_last" ]; then
+        cf="f"
+    fi
+
+    f="$src"
+    if [ "$src" -ot `eval echo \\$$cf` ]; then
+        note "No change since last build."
+        exit 0
+    fi
+
+    cd "$src"
+    for f in $files; do
+        if [ -f "$f" -a ! `eval echo \\$$cf` -nt "$f" ]; then
+            g=`echo "$f" | sed 's/\.md//'`
+            cd "$cwd"
+            # Okay, yeah, this is a dirty hack, but it gets the job done without
+            # having to write the same command twice.
+            bash -xc "markdown_py \"$src/$g.md\" -f \"$dest/$g.html\""
+            if [ $? -ne 0 ]; then
+                error "Build error!"
+                fatal
+            fi
+            cd "$src"
+        fi
+    done
+
+    cd "$cwd"
+    touch .ldtsh_last
+    ;;
+esac
